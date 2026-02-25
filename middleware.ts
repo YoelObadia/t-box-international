7280f9192dc045705a64e4cd2b858fcb72d1f414
Yoel Obadia <yoel.obadia.yo@gmail.com>
2026-02-25 16:37:39 +0200

    route

--- middleware.ts
+++ middleware.ts
@@ -84,5 +84,5 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-    matcher: ['/((?!api|_next/static|_next/image|images|favicon.ico).*)'],
+    matcher: ['/((?!api|_next/static|_next/image|images|favicon.ico|favicon.png|apple-icon.png|logo.jpg).*)'],
 };
